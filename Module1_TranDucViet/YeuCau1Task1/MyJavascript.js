7e74a9661be326facf8ea854083b7ba87e658760
unknown <ducvietqtqb95@gmail.com>
2019-10-15 15:37:57 +0700

    'YeuCau1Task1'

---
+++ Module1_TranDucViet/YeuCau1Task1/MyJavascript.js
@@ -0,0 +1,24 @@
+var Name = prompt("Enter Name Customer:");
+var IdCard = prompt("Enter Id Card Customer:");
+var Birthday = prompt("Enter Birthday Customer:");
+var Email = prompt("Enter Email Customer:");
+var Address = prompt("Enter Address Customer: ");
+var Type = prompt("Enter Type Customer:");
+var Discount = prompt("Enter Discount:");
+var Accompanying = prompt("Enter Number Of Accompanying:");
+var TypeRoom = prompt("Enter Type Room:");
+var RenDays = prompt("Enter Ren Days:");
+var TypeService = prompt("Enter Type Service:");
+alert("Information of Customer"+"\n"
+    + "Name Customer:" + Name + "\n"
+    + "Id Card Customer:" + IdCard + "\n"
+    + "Birthday Customer:" + Birthday + "\n"
+    + "Email Customer:" + Email + "\n"
+    + "Address Customer:" + Address + "\n"
+    + "Type Customer:" + Type + "\n"
+    + "Discount:" + Discount + "\n"
+    + "Number Of Accompanying:" + Accompanying + "\n"
+    + "Type Room:" + TypeRoom + "\n"
+    + "Ren Days:" + RenDays + "\n"
+    + "Type Service:" + TypeService + "\n");
+
